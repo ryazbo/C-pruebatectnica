9de9b899457121525ebe31a0c3fc1a6c948afe40
JuanS <126506142+ryazbo@users.noreply.github.com>
2024-02-06 08:21:26 -0500

    c

---
+++ AñoBisiesto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite su año a evaluar");
+        int YearUsur = Convert.ToInt32(Console.ReadLine());
+        VerifyYear(YearUsur);
+    }
+
+    static void VerifyYear(int num )
+    {
+
+        int isBisi;
+
+        if ((num % 4 == 0 && num % 100 != 0) || (num % 400 == 0))
+        {
+            isBisi = 1;
+        }
+        else
+        {
+            isBisi = 2;
+        }
+        if (isBisi == 1)
+        {
+            Console.WriteLine("el año es biciesto");
+        }
+        if (isBisi == 2)
+        {
+            Console.WriteLine("el año no es biciesto");
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite su numero para hallar dentro de los numeros fibonacci:");
+        int UsrDigit = Convert.ToInt32(Console.ReadLine());
+
+        bool found = IsFibonacci(UsrDigit);
+
+        if (found)
+        {
+            Console.WriteLine($"Su numero {UsrDigit} se encontró dentro de los números fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"Su numero {UsrDigit} no se encontró dentro de los números fibonacci.");
+        }
+    }
+
+    static bool IsFibonacci(int numUser)
+    {
+        if (numUser == 0 || numUser == 1) return true;
+
+        int prev = 0;
+        int current = 1;
+
+        while (current < numUser)
+        {
+            int next = prev + current;
+            prev = current;
+            current = next;
+        }
+
+        return current == numUser;
+    }
+}
